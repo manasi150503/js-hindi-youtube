8a6b208b0133220e858b9c514365ba27d4c52043
manasi150503 <mundemanasi5@gmail.com>
2025-07-21 08:21:50 +0000

    variable execution

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountId = 1445553
+let accountEmail = "manasi@google.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+
+// accountId = 2 // not allowed
+
+accountEmail = "hc@hc.com"
+accountPassword = "21212121"
+accountCity = "Bebgaluru"
+
+console.log(accountId);
+
+console.table([accountId, accountEmail, accountPassword, accountCity])
