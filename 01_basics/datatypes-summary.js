48cbffc4db451d22ac5dc70c9da9473ad8c10b0a
manasi150503 <mundemanasi5@gmail.com>
2025-07-22 06:57:38 +0000

    data type summmarised

--- 01_basics/datatypes-summary.js
+++ 01_basics/datatypes-summary.js
@@ -1,5 +1,7 @@
 // Primitive 
 
+//const { useInsertionEffect } = require("react");
+
 // 7 types : String, Number, Boolean, null, undefined, Symbol, BigInt 
 
 const score = 100
@@ -33,3 +35,31 @@ const myFunction = function(){
 }
 
 console.log(typeof anotherId);
+
+// https://262.ecma-international.org/5.1#sec-11.4.3
+
+
+
+// ++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// Stack (Primitive), Heap (Non-Primitive)
+
+let myYoutubename = 'manasimundedotcom'
+
+let anothername = myYoutubename
+anothername = "chaiaurcode"
+
+console.log(myYoutubename);
+console.log(anothername);
+
+let userOne = {
+    email: "user@google.com",
+    upi: "user@ybl"
+}
+
+let userTwo = userOne
+
+userTwo.email = "manasi@gmail.com",
+
+console.log(userOne.email);
+console.log(userTwo.email);
